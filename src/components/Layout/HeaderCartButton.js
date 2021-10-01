3a8eb931756c5068957824867f3a8cd8346acbce
Michał Gleba <GlebaM@users.noreply.github.com>
2021-10-01 08:43:05 +0200

    UseEffect bump on headBtn once cartItmAmnt changed

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,38 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
-  const numOfCartItems = cartCtx.items.reduce((currNum, item) => {
+
+  const { items } = cartCtx;
+
+  const numOfCartItems = items.reduce((currNum, item) => {
     return currNum + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${
+    buttonIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setButtonIsHighlighted(true);
+    const timer = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+    return function cleanup() {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} {...props}>
+    <button className={btnClasses} {...props}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
